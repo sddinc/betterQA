90b7f37590dbdb070a799b389b6b23bc4ea39934
sddinc <sdinc00@gmail.com>
2021-11-25 22:01:32 +0200

    persembe

--- src/test/java/StepDefinitions/_10_enesStepDef.java
+++ src/test/java/StepDefinitions/_10_enesStepDef.java
@@ -17,8 +17,6 @@ import java.util.List;
 import java.util.Map;
 
 public class _10_enesStepDef {
-
-
     @Given("^user go to  website \"([^\"]*)\"$")
     public void userGoToWebsite(String url)  {
         Driver.getDriver().get(url);
@@ -26,66 +24,17 @@ public class _10_enesStepDef {
     @And("^user click to categories$")
     public void userClickToCategories() throws InterruptedException {
 
-
-
-
-
          List<WebElement> listOfElements = Driver.getDriver().findElements(By.xpath("//div[@id='content']//div//ul//li//a"));
 
-
-
         Thread.sleep(10);
-        //Driver.getDriver().findElement(By.className("btn notification-close")).click();
-       // Driver.waitAndClick(Driver.getDriver().findElement(By.className("btn notification-close")),2);
-       //
-
 
         for (int i = 1; i <=listOfElements.size(); i++) {
-
-
             String menuElements ="(//div[@id='content']//div//ul//li//a)["+i+"]";
-//
-//            Actions actions = new Actions(Driver.getDriver());
-//            actions.moveToElement(Driver.getDriver().findElement(By.xpath(menuElements)));
-//           // actions.click();
-//           actions.perform();
-
-
-
-            Driver.waitAndClick(Driver.getDriver().findElement(By.xpath(menuElements)),5);
+            Driver.hover(Driver.getDriver().findElement(By.xpath(menuElements)));
             Driver.scrollToElement(Driver.getDriver().findElement(By.xpath(menuElements)));
+            Driver.waitAndClick(Driver.getDriver().findElement(By.xpath(menuElements)),5);
             Driver.getDriver().navigate().back();
-
-
         }
-
-
-
-
-
-
-/*
-
-        for (int i = 1; i < 9; i++) {
-
-            List<WebElement> listOfElements = Driver.getDriver().findElements(By.xpath("//li["+i+"]/ul[1]/li[*]"));
-
-            for (int j = 1; j <listOfElements.size() ; j++) {
-
-                String menuElements ="//li["+i+"]/ul[1]/li["+j+"]/a[1]";
-
-                Driver.hover(Driver.getDriver().findElement(By.xpath(menuElements)));
-               Driver.scrollToElement(Driver.getDriver().findElement(By.xpath(menuElements)));
-               // Driver.waitAndClick(Driver.getDriver().findElement(By.xpath(menuElements)),1);
-               // Driver.getDriver().navigate().back();
-            }
-
-        }
-
-*/
-
-
-
 
     }
 
