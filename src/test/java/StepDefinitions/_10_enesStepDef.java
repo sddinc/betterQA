2990b1921bcc20226e174c5ecdbb56211d098868
sddinc <sdinc00@gmail.com>
2021-12-02 22:39:46 +0200

    product name + product price - product image link - patika test 2 resim link alma urunleri gezme

--- src/test/java/StepDefinitions/_10_enesStepDef.java
+++ src/test/java/StepDefinitions/_10_enesStepDef.java
@@ -14,6 +14,8 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.sql.Array;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -24,48 +26,88 @@ public class _10_enesStepDef {
     }
     @And("^user click to categories$")
     public void userClickToCategories() throws InterruptedException {
-
          List<WebElement> listOfElements = Driver.getDriver().findElements(By.xpath("//div[@id='content']//div//ul//li//a"));
          List<WebElement> listOfproduct = Driver.getDriver().findElements(By.xpath("//div[@class='name']//a"));
-        List<WebElement> imageAdress = Driver.getDriver().findElements(By.xpath("//div[@class='image']//a//div//img"));
-    //    Thread.sleep(10);
-//50 left side nr of the products
-        Driver.getDriver().findElement(By.xpath("//button[@class='btn notification-close']")).click();
-        for (int i = 1; i <=30; i++) {
-            String menuElements ="(//div[@id='content']//div//ul//li//a)["+i+"]";
+         List<WebElement> imageAdress = Driver.getDriver().findElements(By.xpath("//div[@class='image']//a//div//img"));
+         List<WebElement> description = Driver.getDriver().findElements(By.xpath("//div[@class='description']"));
+         List<WebElement> price = Driver.getDriver().findElements(By.xpath("//div[@class='price']"));
+         List<WebElement> modelNo = Driver.getDriver().findElements(By.xpath("//span[@class='stat-2']//span"));
+
+          Driver.getDriver().findElement(By.xpath("//button[@class='btn notification-close']")).click();
+int maxnr=0;
+
+          int arr[]= new int[listOfElements.size()];
+        for (int a = 1; a < 3; a++) {
+            if(a==29){
+                a++;
+            }
+            String menuElements ="(//div[@id='content']//div//ul//li//a)["+a+"]";
+            System.out.println("a="+a);
             Driver.waitAndClick(Driver.getDriver().findElement(By.xpath(menuElements)),5);
             Driver.waitAndClick(Driver.getDriver().findElement(By.cssSelector("button#btn-list-view")),5);
-
-
-
-
             String limit ="#input-limit";
             Select limitProduct= new Select(Driver.getDriver().findElement(By.cssSelector(limit)));
             limitProduct.selectByVisibleText("100");
 
-            System.out.println("-----------------------"+Driver.getDriver().findElement(By.xpath(" //h1[@class='title page-title']")).getText());
+
+            boolean cv= Driver.waitForInVisibility(By.cssSelector("(//h3[@class='title module-title'])[2]"),5);
+            Driver.getDriver().findElement(By.xpath("//h3[@class='title module-title'])[2]")).isDisplayed()
+            int b=1;
+            while(!cv){
+              if(cv){
+                    Driver.getDriver().navigate().back();
+                }
+
+                String productName ="(//div[@class='name']//a)["+b+"]";
+                b++;
+                System.out.println("b="+b);
+                Driver.scrollToElement(Driver.getDriver().findElement(By.xpath(productName)));
+
+
+        }
+            arr[a]=b;
+            System.out.println("array index="+arr[a]);
+            Driver.getDriver().navigate().back();
+        }
 
 
 
 
 
- for (int j = 1; j <=imageAdress.size(); j++) {
 
 
+
+        for (int i = 1; i <=3; i++) {
+            if(i==29){
+                i++;
+            }
+            String menuElements ="(//div[@id='content']//div//ul//li//a)["+i+"]";
+            System.out.println("i="+i);
+            Driver.waitAndClick(Driver.getDriver().findElement(By.xpath(menuElements)),5);
+            Driver.waitAndClick(Driver.getDriver().findElement(By.cssSelector("button#btn-list-view")),5);
+            String limit ="#input-limit";
+            Select limitProduct= new Select(Driver.getDriver().findElement(By.cssSelector(limit)));
+            limitProduct.selectByVisibleText("100");
+            System.out.println("----------"+Driver.getDriver().findElement(By.xpath(" //h1[@class='title page-title']")).getText());
+
+
+            for (int j = 1; j <=arr[i]; j++) {
 
                 String productName ="(//div[@class='name']//a)["+j+"]";
-                String imageAdressLink ="(//div[@class='image']//a//div//img)["+j+"]";
+                String imageAdressLink ="(//div[@class='product-thumb']//div//img)["+j+"]";
+                String descriptionList ="(//div[@class='description'])["+j+"]";
+                String priceList ="(//div[@class='price'])["+j+"]";
+                String modelList ="(//span[@class='stat-2'])["+j+"]";
 
-
-
-
+                System.out.println("j="+j);
+               Driver.scrollToElement(Driver.getDriver().findElement(By.xpath(productName)));
                 System.out.println(Driver.getDriver().findElement(By.xpath(productName)).getText());
-                System.out.println(Driver.getDriver().findElement(By.xpath(imageAdressLink)).getAttribute("src"));
-
+                System.out.println(Driver.getDriver().findElement(By.xpath(descriptionList)).getText());
+                System.out.println(Driver.getDriver().findElement(By.xpath(priceList)).getText());
+                System.out.println(Driver.getDriver().findElement(By.xpath(modelList)).getText());
+               System.out.println(Driver.getDriver().findElement(By.xpath(imageAdressLink)).getAttribute("src"));
 
             }
-
-
             Driver.getDriver().navigate().back();
         }
 
